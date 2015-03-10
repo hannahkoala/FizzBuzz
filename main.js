bc0741828c56dc5b27c807c5d042cf259590276d
Hannah <hannah_koala@Hannahs-MacBook-Pro.local>
2015-03-10 16:02:37 -0500

    FizzBuzz
    Gave this cliche a whirl

---
+++ main.js
@@ -0,0 +1,11 @@
+function fizzBuzz(i){
+  var result = "";
+  if (!(i%3)) {result += "Fizz"};
+  if (!(i%5)) {result += "Buzz"};
+  if (result == !("string")) {result = i};
+  return result
+};
+
+for(var i = 1; i <= 100; i++){
+  console.log(fizzBuzz(i));
+};
