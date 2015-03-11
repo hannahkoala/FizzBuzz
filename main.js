129c26605e5d928f66af7651f8cd467b6606f175
Hannah <hannah_koala@Hannahs-MacBook-Pro.local>
2015-03-10 23:56:39 -0500

    Made code more clear
    Stopped abusing falsey string result

--- main.js
+++ main.js
@@ -2,10 +2,10 @@ function fizzBuzz(i){
   var result = "";
   if (!(i%3)) {result += "Fizz"};
   if (!(i%5)) {result += "Buzz"};
-  if (result == !("string")) {result = i};
+  if (result === ("")) {result = i};
   return result
 };
 
 for(var i = 1; i <= 100; i++){
-  console.log(fizzBuzz(i));
+  console.log(fizzBuzz(i));;
 };
